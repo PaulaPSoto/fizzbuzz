9f282eef345bb0327644b955840c3069c1f6c7ca
PaulaPSoto <paezsotopaula@gmail.com>
2021-09-30 18:16:33 +0200

    Add multiple of 3 and 5

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,43 @@
+const fizzbuzz = require("./fizzbuzz");
+
+//Esperamos recibir, el resultado de nuestra operación es fizzbuzz(1).
+//Por último comprobación
 describe("fizzbuzz", () => {
-  test("test", () => {
-    expect(true).toBe(true);
+  test("should print 1 if receive 1", () => {
+    const expected = 1;
+    const result = fizzbuzz(1);
+    expect(expected).toBe(result);
+  });
+
+  //Cuando el mumero sea múltipo de 3 imprima fizz
+
+  test("sholud print fizz if they receive 3", () => {
+    const expected = "fizz";
+    const result = fizzbuzz(3);
+    expect(expected).toBe(result);
+  });
+
+  test("should print fizz if they receive a multiple of 3", () => {
+    const expected = "fizz";
+    const result = fizzbuzz(6);
+    expect(expected).toBe(result);
+  });
+
+  test("should print buzz if they receive 5", () => {
+    const expected = "buzz";
+    const result = fizzbuzz(5);
+    expect(expected).toBe(result);
+  });
+
+  test("should print buzz if they receive a multiple of 5", () => {
+    const expected = "buzz";
+    const result = fizzbuzz(10);
+    expect(expected).toBe(result);
+  });
+
+  test("should print fizzbuzz if they receive a multiple of 3 and 5", () => {
+    const expected = "fizzbuzz";
+    const result = fizzbuzz(15);
+    expect(expected).toBe(result);
   });
 });
